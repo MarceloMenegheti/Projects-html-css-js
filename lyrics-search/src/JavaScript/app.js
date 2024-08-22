d28149f2ed88d30e27d41347f955b5de59d42ac3
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-08-21 22:23:11 -0300

    Update project

--- lyrics-search/src/JavaScript/app.js
+++ lyrics-search/src/JavaScript/app.js
@@ -5,11 +5,28 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiUrl = `https://api.lyrics.ovh`
 
+const insertSongsIntoPage = songsInfo => {
+    console.log(songsInfo);
+    songsContainer.innerHTML = ``
+}
+
+const fetchSongs = async term => {
+    const Response = await fetch(`${apiUrl}/suggest/${term}`)
+    const data = await Response.json()
+
+    insertSongsIntoPage(data)
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault()
 
-    const searchTerm = searchInput.value
+    const searchTerm = searchInput.value.trim()
 
-    console.log(searchTerm);
+    if(!searchTerm){
+        songsContainer.innerHTML =`<li class="warning-message">Por favor, digite um termo valido</li>` 
+        return
+    }
+
+    fetchSongs(searchTerm)
 })
-console.log({form, searchInput, songsContainer, prevAndNextContainer});
+//console.log({form, searchInput, songsContainer, prevAndNextContainer});
