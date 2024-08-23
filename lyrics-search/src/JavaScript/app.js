7c0600b037433d46b6005a305e2acd0c311e3389
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-08-22 23:26:43 -0300

    Update

--- lyrics-search/src/JavaScript/app.js
+++ lyrics-search/src/JavaScript/app.js
@@ -5,10 +5,32 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiUrl = `https://api.lyrics.ovh`
 
-const insertSongsIntoPage = songsInfo => {
-    console.log(songsInfo);
-    songsContainer.innerHTML = ``
+const getMoreSongs = async url => {
+    const Response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await Response.json()
+    console.log(data);
+    
+    insertSongsIntoPage(data)
 }
+
+const insertSongsIntoPage = songsInfo => {
+   songsContainer.innerHTML = songsInfo.data.map(song => `
+    <li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
+    </li>
+    `).join('')
+
+    if(songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+            ${songsInfo.prev ? `<Button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+            ${songsInfo.next ? `<Button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+        return
+    } 
+
+    prevAndNextContainer.innerHTML = ''
+}   
 
 const fetchSongs = async term => {
     const Response = await fetch(`${apiUrl}/suggest/${term}`)
@@ -28,5 +50,9 @@ form.addEventListener('submit', event => {
     }
 
     fetchSongs(searchTerm)
+})
+
+songsContainer.addEventListener('click', event => {
+    console.log(event);  
 })
 //console.log({form, searchInput, songsContainer, prevAndNextContainer});
